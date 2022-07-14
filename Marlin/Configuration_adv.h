3f28783e87bf64bf46fb8a43703edf4c4a05e766
itfabricacion <administracion@itfabricacion.com>
2022-07-14 09:31:06 +0200

    Feedrate and CURRENT

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2214,8 +2214,8 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-#define MINIMUM_STEPPER_POST_DIR_DELAY 60  //650
-#define MINIMUM_STEPPER_PRE_DIR_DELAY 60   //650
+//#define MINIMUM_STEPPER_POST_DIR_DELAY 60  //650
+//#define MINIMUM_STEPPER_PRE_DIR_DELAY 60   //650
 
 /**
  * Minimum stepper driver pulse width (in µs)
@@ -2228,7 +2228,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-#define MINIMUM_STEPPER_PULSE 2
+//#define MINIMUM_STEPPER_PULSE 2
 
 /**
  * Maximum stepping rate (in Hz) the stepper driver allows
@@ -2709,7 +2709,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       1250       // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       800       // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE         0.075     //0.11
@@ -2729,7 +2729,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       1250
+    #define Y_CURRENT       800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.075   //0.11
@@ -2749,7 +2749,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       2250
+    #define Z_CURRENT       1600
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.075    //0.11
@@ -2759,7 +2759,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z2)
-    #define Z2_CURRENT      2250
+    #define Z2_CURRENT      1600
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS    Z_MICROSTEPS
     #define Z2_RSENSE         0.075   //0.11
@@ -2819,7 +2819,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      2250
+    #define E0_CURRENT      950
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.075   //0.11
     #define E0_CHAIN_POS     -1
