7ad917723ef306eb24924928086dc61e3c1201fa
itfabricacion <administracion@itfabricacion.com>
2022-08-23 12:22:28 +0200

    Changed Current for all Axis

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2709,7 +2709,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       800       // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       900       // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE         0.075     //0.11
@@ -2729,7 +2729,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       800
+    #define Y_CURRENT       900
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.075   //0.11
