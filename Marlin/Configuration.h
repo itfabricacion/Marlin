3a9da4bd1c70192cf19f11586fea70bdd0bd89a1
itfabricacion <administracion@itfabricacion.com>
2022-09-21 12:52:17 +0200

    Update E DEFAULT_AXIS_STEPS_PER_UNIT

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -966,7 +966,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 431.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 385.27 }
 
 /**
  * Default Max Feed Rate (mm/s)
