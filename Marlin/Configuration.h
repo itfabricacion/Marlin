2aa93419f13547a96f4e59372c187c4c793d2967
unknown <administracion@itfabricacion.com>
2022-11-15 12:51:12 +0100

    ITMAX01

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1017,7 +1017,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 385.27 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 418.77 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
