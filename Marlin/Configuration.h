b8606bc0cc225fa9b06b03a384ae1b2abd5d38ae
CarlosAlbaLopez <administracion@itfabricacion.com>
2023-10-20 10:29:09 +0200

    Changes for BondTech Pro extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -966,7 +966,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 415 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 410 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1390,7 +1390,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
